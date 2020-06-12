9628d5c609f812044e933cfc1af6f3bd1c122992
KevinStivenP <kevin.pena@accenture.com>
2020-06-12 18:51:20 -0500

    Test MVVMCross

---
+++ MVVMCrossTest/TipCalculator.Core/App.cs
@@ -0,0 +1,19 @@
+﻿using MvvmCross.IoC;
+using MvvmCross.ViewModels;
+using TipCalculator.Core.ViewModels;
+
+namespace TipCalculator.Core
+{
+    public class App : MvxApplication
+    {
+        public override void Initialize()
+        {
+            CreatableTypes()
+                .EndingWith("Service") // Inyección de Dependencias, todo lo que termine en services lo va inyectar
+                .AsInterfaces()
+                .RegisterAsLazySingleton();
+
+            RegisterAppStart<TipViewModel>(); //Por donde arranca el proyecto
+        }
+    }
+}
